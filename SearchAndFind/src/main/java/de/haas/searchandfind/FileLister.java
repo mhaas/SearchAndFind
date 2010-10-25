745aa5a4c0d187cc59a9cbcb0340dd328091b05e
laga <laga@363a596b-6ee8-4a37-ab8d-e0bad386b831>
2010-10-25 15:06:10 +0000

    Commit FileLister and Indexer. Needs more fleshing out.
    git-svn-id: file:///home/laga/uni/WS1011/ir/repo@4 363a596b-6ee8-4a37-ab8d-e0bad386b831

---
+++ SearchAndFind/src/main/java/de/haas/searchandfind/FileLister.java
@@ -0,0 +1,90 @@
+
+
+package de.haas.searchandfind;
+
+import java.io.File;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Michael Haas <haas@cl.uni-heidelberg.de>
+ */
+public class FileLister extends Thread {
+
+    private BlockingQueue<File> queue;
+    private File dir;
+    private final static int QUEUE_CAPACITY = 500;
+    /**
+     * Constructor. Use this to pass in an external queue.
+     *
+     * @param fileQueue Queue where files are inserted
+     * @param directory Directory for which files are to be listed
+     */
+    public FileLister(BlockingQueue<File> fileQueue, File directory) {
+        this.queue = fileQueue;
+        this.dir = directory;
+    }
+
+    /**
+     * Constructor. Use this to let the FileLister create its own queue.
+     * 
+     * @param directory Directory for which files are to be listed
+     */
+    public FileLister(File directory) {
+        this(new LinkedBlockingQueue<File>(QUEUE_CAPACITY), directory);
+    }
+
+    /**
+     * Starts file listing.
+     */
+   @Override
+   public void run() {
+        if (this.dir.isDirectory()) {
+            try {
+                this.listFiles(this.dir);
+            } catch (InterruptedException ex) {
+                // TODO: do something about this.
+                Logger.getLogger(FileLister.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else {
+            throw new RuntimeException("dir field is not a directory.");
+        }
+
+
+   }
+
+   /**
+    * Lists files recursively. Enqueues normal files into queue.
+    *
+    * @param directory directory to be listed recursively
+    * @throws InterruptedException if something interrupts enqueue operation
+    */
+   private void listFiles(final File directory) throws InterruptedException {
+
+       File[] children = directory.listFiles();
+       for (int ii = 0; ii < children.length; ii++) {
+           File currentFile = children[ii];
+           // if directory, recurse. if file, enqueue
+           if (currentFile.isDirectory()) {
+               this.listFiles(currentFile);
+           } else if (currentFile.isFile()) {
+               // enqueue
+               this.queue.put(currentFile);
+           }
+       }
+
+   }
+
+   /**
+    * Getter for queue field.
+    *
+    * @return Queue for files
+    */
+   public BlockingQueue<File> getQueue() {
+       return this.queue;
+   }
+
+}
---
+++ SearchAndFind/src/main/java/de/haas/searchandfind/Indexer.java
@@ -0,0 +1,65 @@
+package de.haas.searchandfind;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.store.SimpleFSDirectory;
+import org.apache.lucene.util.Version;
+
+/**
+ *
+ * @author Michael Haas <haas@cl.uni-heidelberg.de>
+ */
+public class Indexer {
+
+    private static final File DIRECTORY = new File("/home/laga/uni/");
+    private static final File INDEX = new File("/tmp/lucene/");
+    // TODO: sensible value
+    private static final int MAX_FIELD_LENGTH = 500;
+    private BlockingQueue<File> queue = new LinkedBlockingQueue<File>();
+    // TODO: is an IndexWriter threadsafe? Or do we need a singleton here?
+    // "An IndexWriter creates and maintains an index. "
+
+    private void kickOffIndexing() throws IOException, InterruptedException {
+
+        // where we store the index on disk
+        Directory directory = new SimpleFSDirectory(INDEX);
+        // this is used to analyze/tokenize fields
+        // apparently, it's not used for all fields
+        // TODO: read up on that
+        Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_30);
+        IndexWriter.MaxFieldLength mfl = new IndexWriter.MaxFieldLength(MAX_FIELD_LENGTH);
+        IndexWriter writer = new IndexWriter(directory, analyzer, mfl);
+
+
+
+        FileLister f = new FileLister(this.queue, Indexer.DIRECTORY);
+        f.start();
+        // loop endlessly.
+        // possibly add support for poison element in queue later on
+        // OTOH, it's quite possible we want to keep indexing with the live
+        // indexer forever and ever
+        while (true) {
+            File file = this.queue.take();
+            Document document = this.dispatchToFileIndexer(file);
+            writer.addDocument(document);
+        }
+    }
+
+    /**
+     * Invokes
+     *
+     * @param f
+     * @return
+     */
+    private Document dispatchToFileIndexer(File f) {
+        return null;
+    }
+}
