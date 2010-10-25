1c30f79b0e44dca513e3cc3cd707c27909fbe795
laga <laga@363a596b-6ee8-4a37-ab8d-e0bad386b831>
2010-10-25 12:39:59 +0000

    Initial import.
    git-svn-id: file:///home/laga/uni/WS1011/ir/repo@2 363a596b-6ee8-4a37-ab8d-e0bad386b831

---
+++ SearchAndFind/src/main/java/de/haas/searchandfind/App.java
@@ -0,0 +1,13 @@
+package de.haas.searchandfind;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
