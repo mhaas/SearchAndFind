66fc9fe1c8cdbc2230620146d54e4e2b2272f8c1
laga <laga@363a596b-6ee8-4a37-ab8d-e0bad386b831>
2010-10-29 09:17:05 +0000

    Use constants for field names
    git-svn-id: file:///home/laga/uni/WS1011/ir/repo@12 363a596b-6ee8-4a37-ab8d-e0bad386b831

--- SearchAndFind/src/main/java/de/haas/searchandfind/backend/documentgenerator/TextFileDocumentGenerator.java
+++ SearchAndFind/src/main/java/de/haas/searchandfind/backend/documentgenerator/TextFileDocumentGenerator.java
@@ -2,9 +2,11 @@
 
 package de.haas.searchandfind.backend.documentgenerator;
 
+import de.haas.searchandfind.backend.App;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.lucene.document.DateTools.Resolution;
@@ -18,26 +20,25 @@ import org.apache.lucene.document.NumericField;
  */
 public class TextFileDocumentGenerator implements FileDocumentGenerator {
 
-    private static class Fieldable {
-
-        public Fieldable() {
-        }
-    }
-
     @Override
     public Document makeDocument(File i) {
         
         Document doc = new Document();
+        try {
+            Field fileNameField = new Field(App.FIELD_FILE_NAME, i.getCanonicalPath(), Field.Store.YES, Field.Index.NOT_ANALYZED);
+            doc.add(fileNameField);
+        } catch (IOException ex) {
+            Logger.getLogger(TextFileDocumentGenerator.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
-        NumericField lastModField = new NumericField("lastModified");
+        NumericField lastModField = new NumericField(App.FIELD_LAST_MODIFIED);
         lastModField.setLongValue(i.lastModified());
         // last-modified date
-
-        
         doc.add(lastModField);
+
         try {
             // TODO: how is the content indexed now?
-            Field contentField = new Field("content", new FileReader(i));
+            Field contentField = new Field(App.FIELD_CONTENT, new FileReader(i));
             doc.add(contentField);
         } catch (FileNotFoundException ex) {
             Logger.getLogger(TextFileDocumentGenerator.class.getName()).log(Level.SEVERE, null, ex);
